64e1e4affa372805bd99db2feeadc2efb6f6dadf
Brenda Judith <301510275@bis.ute.edu.mx>
2017-03-15 17:32:53 -0600

    guardar y mostrar vestidos codigo

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,41 @@
 class AdminController < ApplicationController
   
-  
+ 
   def listavestido
+
+    uri = URI.parse('https://ellasboutique.000webhostapp.com/venta/viewAll.php')
+
+    descripcion= params[:des]
+    estilo= params[:estilo]
+    talla= params[:talla]
+    precio= params[:precio]
+    cantidad= params[:cantidad]
+    imagen= params[:img]
+
+    params= {:des => descripcion, :estilo => estilo,:talla => talla,:precio => precio,:cantidad => cantidad,:img => imagen}
+    uri.query = URI.encode_www_form(params)
+
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE  
+    res = http.request(request)
+
+   
+    object = res.body if res.is_a?(Net::HTTPSuccess) 
+
+    object = JSON.parse(respuesta)
+
+    #if object.length >= 0 then
+    
+     
+   
+      
+    #else 
+       
+     
+    #end
   end
 
   def idvestido
@@ -14,7 +48,36 @@ class AdminController < ApplicationController
   end
 
   def guardarv
- 
+     uri = URI.parse('https://ellasboutique.000webhostapp.com/vestido/insertVestido.php')
+
+    descripcion= params[:des]
+    estilo= params[:estilo]
+    talla= params[:talla]
+    precio= params[:precio]
+    cantidad= params[:cantidad]
+    imagen = params[:imagen]
+    
+    params= {:des => descripcion, :estilo => estilo,:talla => talla,:precio => precio,:cantidad => cantidad,:imagen => imagen}
+    uri.query = URI.encode_www_form(params)
+
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE  
+    res = http.request(request)
+
+   
+    object = res.body if res.is_a?(Net::HTTPSuccess) 
+
+     
+
+   if object.length >= 0 then
+     
+            succes = "";
+         else
+            error = "";
+         end
   end
 
   def borrarp
@@ -25,6 +88,15 @@ class AdminController < ApplicationController
 
   def buscarp
   end
+
+  
+   
+
+   def listar_imagenes
+   end
+
+   def borrar_imagenes
+   end
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -17,21 +17,7 @@ Rails.application.routes.draw do
 
   get 'admin/index'
 
-  get 'listavestido/idvestido'
-
-  get 'listavestido/borrarv'
-
-  get 'listavestido/actualizarv'
-
-
-
-  get 'listavestido/borrarp'
-
-  get 'listavestido/listapedido'
-
-  get 'listavestido/buscarp'
-
-  get 'listavestido/index'
+  post 'admin/guardarv'
 
   post 'login/create'
 
@@ -40,11 +26,7 @@ Rails.application.routes.draw do
   get 'login/index'
 
 
-  resources :admin, only: [:index, :guardarv] do
-    collection do
-         get 'listavestido/guardarv'
-    end
-end
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
