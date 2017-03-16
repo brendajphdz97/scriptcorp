45fe4f3cfd005789adc7dfccbae1f52a60d74cc5
KikeRdz <enrique.johanny@gmail.com>
2017-03-16 00:20:32 -0600

    render de tabla con paginado

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
-  
- 
+
+
   def listavestido
 
     uri = URI.parse('https://ellasboutique.000webhostapp.com/vestido/viewAll.php')
@@ -15,32 +15,20 @@ class AdminController < ApplicationController
     params= {:descripcion => descripcion, :estilo => estilo,:talla => talla,:precio => precio,:cantidad => cantidad,:imagen => imagen}
     uri.query = URI.encode_www_form(params)
 
-    
+
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE  
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     res = http.request(request)
 
-   
-    respuesta = res.body if res.is_a?(Net::HTTPSuccess) 
 
-    $result = JSON.parse(respuesta)
+    respuesta = res.body if res.is_a?(Net::HTTPSuccess)
 
-     if $result then
-    
-     
-   
-      
-    else 
-       
-     
-    end
-
-  
+    $result = respuesta #JSON.parse(respuesta)  
   end
 
-  
+
 
   def idvestido
   end
@@ -60,24 +48,24 @@ class AdminController < ApplicationController
     precio= params[:precio]
     cantidad= params[:cantidad]
     imagen = params[:imagen]
-    
+
     params= {:des => descripcion, :estilo => estilo,:talla => talla,:precio => precio,:cantidad => cantidad,:imagen => imagen}
     uri.query = URI.encode_www_form(params)
 
-    
+
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.request_uri)
     http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE  
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     res = http.request(request)
 
-   
-    object = res.body if res.is_a?(Net::HTTPSuccess) 
 
-     
+    object = res.body if res.is_a?(Net::HTTPSuccess)
+
+
 
    if object.length >= 0 then
-     
+
             succes = "";
          else
             error = "";
@@ -93,8 +81,8 @@ class AdminController < ApplicationController
   def buscarp
   end
 
-  
-   
+
+
 
    def listar_imagenes
    end
@@ -105,4 +93,3 @@ class AdminController < ApplicationController
   def index
   end
 end
-
