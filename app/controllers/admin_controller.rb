7e81a0c1889436ff3fe832c95c0e20ae97cf42f7
KikeRdz <enrique.johanny@gmail.com>
2017-03-25 16:22:58 -0600

    correccion codigo repetido

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,60 +2,24 @@ class AdminController < ApplicationController
 
 
   def listavestido
-
     if params[:id] == nil then
-      uri = URI.parse('https://ellasboutique.000webhostapp.com/vestido/viewAll.php')
-      #Como los parametros que se pasaron desde html se llaman igual que los que hay
-      #que mandar al api entonces se manda el objeto params directo
-      #descripcion= params[:descripcion]
-      #estilo= params[:estilo]
-      #talla= params[:talla]
-      #precio= params[:precio]
-      #cantidad= params[:cantidad]
-      #imagen= params[:imagen]
-      #params= {:descripcion => descripcion, :estilo => estilo,:talla => talla,:precio => precio,:cantidad => cantidad,:imagen => imagen}
+      # params: descripcion estilo talla precio cantidad imagen
+      response = consume_api('https://ellasboutique.000webhostapp.com/vestido/viewAll.php', params, "GET")
     else
-      uri = URI.parse('https://ellasboutique.000webhostapp.com/vestido/viewOne.php')
-      #id = params[:id]
-      #params= {:id => id}
+      #params[:id]
+      response = consume_api('https://ellasboutique.000webhostapp.com/vestido/viewOne.php', params, "GET")
     end
-    uri.query = URI.encode_www_form(params)
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    res = http.request(request)
-
-    object = res.body if res.is_a?(Net::HTTPSuccess)
-    $result = object
-
+    $result = response.body if response.is_a?(Net::HTTPSuccess)
   end
-
-
 
   def idvestido
     #No se requiere
   end
 
   def borrarv
-     uri = URI.parse('https://ellasboutique.000webhostapp.com/vestido/deleteVestido.php')
-
-    id= params[:id]
-
-
-    params= {:id => id}
-    uri.query = URI.encode_www_form(params)
-
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    res = http.request(request)
-
-    if res.is_a?(Net::HTTPSuccess) then
-      redirect_to admin_listavestido_url :notice => "Se elimino correctamente #{id}"
+    response = consume_api('https://ellasboutique.000webhostapp.com/vestido/deleteVestido.php', params, "POST")
+    if response.is_a?(Net::HTTPSuccess) then
+      redirect_to admin_listavestido_url :notice => "Se elimino correctamente #{params[:id]}"
     else
       redirect_to admin_listavestido_url :notice => "Ocurrio un error"
     end
@@ -65,8 +29,6 @@ class AdminController < ApplicationController
   end
 
   def guardarv
-    uri = URI.parse('https://ellasboutique.000webhostapp.com/vestido/insertVestido.php')
-
     descripcion= params[:descripcion]
     estilo= params[:estilo]
     talla= params[:talla]
@@ -74,23 +36,21 @@ class AdminController < ApplicationController
     cantidad= params[:cantidad]
     imagen = params[:imagen]
 
-    params= {:descripcion => descripcion, :estilo => estilo,:talla => talla,:precio => precio,:cantidad => cantidad,:imagen => imagen}
+    response = consume_api('https://ellasboutique.000webhostapp.com/vestido/insertVestido.php', {
+      :descripcion => descripcion,
+      :estilo => estilo,
+      :talla => talla,
+      :precio => precio,
+      :cantidad => cantidad,
+      :imagen => imagen
+    }, "POST")
 
-    uri.query = URI.encode_www_form(params)
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    res = http.request(request)
-
-    case res
+    case response
       when Net::HTTPSuccess, Net::HTTPRedirection
         #Ok
       else
-        res.value
-        end
-
+        response.value
+    end
   end
 
   def borrarp
@@ -112,5 +72,25 @@ class AdminController < ApplicationController
    end
 
   def index
+  end
+
+  private
+  def consume_api(url, params, method)
+    uri = URI.parse(url)
+    uri.query = URI.encode_www_form(params)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    if(method == "POST") then
+      request = Net::HTTP::Post.new(uri.request_uri)
+    else
+      request = Net::HTTP::Get.new(uri.request_uri)
+    end
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    res = http.request(request)
+
+    object = res.body if res.is_a?(Net::HTTPSuccess)
+
+    return res
   end
 end
